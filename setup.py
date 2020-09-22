881b81cf3cb5ed875b5805e1076bdae375104ee7
christian_vadillo <christianvadillo@hotmail.com>
2020-09-22 18:47:26 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Works done in the course',
+    author='Christian Vadillo',
+    license='MIT',
+)
